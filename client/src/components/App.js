7c56915c9e9c4ba19e59da928c8013d134dd9ebd
Josh Kay <josh.m.kay@gmail.com>
2019-03-19 00:56:33 -0400

    Changed navigation drawer style.  Added theme to material UI.  Added dark logo for navigation back to landing page.  Added logo to NHL Scores sidebar link.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import deepPurple from '@material-ui/core/colors/deepPurple';
 import { DatePicker, MuiPickersUtilsProvider } from "material-ui-pickers";
 import MomentUtils from '@date-io/moment';
 
-import PersistentDrawerLeft from './PersistentDrawerLeft';
+import ResponsiveDrawer from './navigation/ResponsiveDrawer';
 import LandingPage from '../pages/LandingPage';
 import NHLScoresPage from '../pages/NHLScoresPage';
 
@@ -13,23 +15,35 @@ import configureStore from '../store';
 
 const store = configureStore();
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: grey[900] },
+    secondary: { main: deepPurple[600] }
+  },
+  typography: {
+    useNextVariants: true,
+  }
+});
+
 class App extends Component 
 {
   render() 
   {
     return (
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <BrowserRouter>
-          <Provider store={store}>
-            <PersistentDrawerLeft>
-              <Switch>
-                <Route exact path='/' component={LandingPage} />
-                <Route path='/nhl/scores' component={NHLScoresPage} />
-              </Switch>
-            </PersistentDrawerLeft>
-          </Provider>
-        </BrowserRouter>
-      </MuiPickersUtilsProvider>
+      <MuiThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <BrowserRouter>
+            <Provider store={store}>
+              <ResponsiveDrawer>
+                <Switch>
+                  <Route exact path='/' component={LandingPage} />
+                  <Route path='/nhl/scores' component={NHLScoresPage} />
+                </Switch>
+              </ResponsiveDrawer>
+            </Provider>
+          </BrowserRouter>
+        </MuiPickersUtilsProvider>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ client/src/components/navigation/ResponsiveDrawer.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+import { Link } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: 20,
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'center',
+    ...theme.mixins.toolbar
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+  drawerLink: {
+    textDecoration: 'none'
+  },
+  drawerLinkIcon: {
+    width: '1em',
+    height: '1em',
+    fontSize: '24px',
+    display: 'inline-block',
+    userSelect: 'none',
+    flexShrink: 0
+  },
+  logo: {
+    alignSelf: 'center'
+  }
+});
+
+class ResponsiveDrawer extends React.Component 
+{
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar}>
+          <Link to={'/'} className={classes.logo}>
+            <img src={`${process.env.PUBLIC_URL}/favicon-dark.ico`} />
+          </Link>
+        </div>
+        <Divider />
+        <List>
+          <Link to={'/nhl/scores'} className={classes.drawerLink}>
+            <ListItem button>
+              <ListItemIcon>
+                <img className={classes.drawerLinkIcon} 
+                  src="https://www-league.nhlstatic.com/images/logos/league-light/133.svg" />
+              </ListItemIcon>
+              <ListItemText primary="NHL Scores" />
+            </ListItem>
+          </Link>
+        </List>
+      </div>
+    );
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" noWrap>
+              Puck Luck
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav className={classes.drawer}>
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={this.props.container}
+              variant="temporary"
+              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+              open={this.state.mobileOpen}
+              onClose={this.handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          { this.props.children }
+        </main>
+      </div>
+    );
+  }
+}
+
+ResponsiveDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
